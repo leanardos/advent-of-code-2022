1303aed702591ab67ad4843024e5c781ce8ba598
Anar Sadigli <anar.sadigli@coolbet.com>
2022-12-03 00:20:44 +0200

    part-2 cleanup

--- day-1/part-2.ts
+++ day-1/part-2.ts
@@ -1,22 +1,19 @@
 const input = await Deno.readTextFile("./input.txt");
 const inputArray = input.split("\n");
 
-const totalArray = []
+const allElfCaloriesInTotalArray = []
 while(inputArray.length > 0) {
-    totalArray.push( 
-		inputArray
-			.splice(
-				0,
-				inputArray.indexOf('\r') + 1 <= 0
-					? inputArray.length
-					: inputArray.indexOf('\r') + 1
-			)
-			.map((item) => +item)
-			.reduce((acc, currentItem) => acc + currentItem, 0));
+    const indexOfNewLine = inputArray.indexOf("\r") + 1;
+	const eachElfCalorieArray = inputArray
+		.splice(0, indexOfNewLine <= 0 ? inputArray.length : indexOfNewLine)
+		.map((item) => +item);
+
+	const eachElfCaloriesTotal = eachElfCalorieArray.reduce((acc, currentItem) => acc + currentItem, 0);
+    allElfCaloriesInTotalArray.push(eachElfCaloriesTotal);
 
 }
 
-const descendingSortedArray = totalArray.sort((a, b) => b - a);
+const descendingSortedArray = allElfCaloriesInTotalArray.sort((a, b) => b - a);
 const topThreeTotal = descendingSortedArray.splice(0, 3);
 const totalOfTopThree = topThreeTotal.reduce((acc, curr) => acc + curr, 0);
 console.log({ totalOfTopThree });
