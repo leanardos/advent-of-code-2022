9d1e63ae3ec018c256963e762dff7856a3cc0523
Anar Sadigli <anar.sadigli@coolbet.com>
2022-12-16 23:31:25 +0200

    day four - part one solved

---
+++ day-4/part-1.ts
@@ -0,0 +1,26 @@
+const input = await Deno.readTextFile('./input.txt');
+const trimmedInput = input.replace('\r', '');
+const inputArray = trimmedInput.split("\n");
+
+let counter = 0;
+inputArray.forEach(input => {
+    const firstPairFirstItem = +input.split(',')[0].replace('\r', '').split('-')[0];
+    const firstPairSecondItem = +input.split(',')[0].replace('\r', '').split('-')[1];
+    
+    const secondPairFirstItem = +input.split(',')[1].replace('\r', '').split('-')[0];
+    const secondPairSecondItem = +input.split(',')[1].replace('\r', '').split('-')[1];
+
+    if (firstPairFirstItem >= secondPairFirstItem && firstPairSecondItem <= secondPairSecondItem) {
+        console.log('--------------: ',input); // overlapping pairs
+        counter ++;
+    }
+
+    else if (firstPairFirstItem <= secondPairFirstItem && firstPairSecondItem >= secondPairSecondItem) {
+        console.log('--------------: ', input); // overlapping pairs
+        counter ++;
+    } else {
+        console.log(input);
+    }
+
+});
+console.log({ counter });
